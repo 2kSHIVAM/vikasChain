b66734b618f9196246956c9324367eabc4ad9460
Shivam <shivamsinghrajput1357@gmail.com>
2023-04-28 23:53:28 +0530

    server made faster

--- backend_server/server.js
+++ backend_server/server.js
@@ -20,42 +20,52 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-async function loadModel(results) {
-  const model = await tf.loadLayersModel(
+var model;
+async function load() {
+  console.log("hello");
+  model = await tf.loadLayersModel(
     "https://nikhil2904.github.io/testibg/model.json"
   );
+  console.log("hello");
+}
+load();
 
+async function loadModel(results) {
+  console.log("entered load function");
+
+  console.log("json loaded");
   const oneDArray = tf.tensor1d(results);
   const shape = [1, 128, 128, 3];
   const newArray = oneDArray.reshape(shape);
-
+  console.log("4d work done");
   // Log the new 4D array to the console
   console.log(newArray);
+  console.log("prediction started");
 
   const op = model.predict(newArray);
   const opData = op.dataSync();
   const number = Number(opData[0]);
-
+  console.log(number);
+  console.log("prediction ended");
   // console.log(tensor4d.shape);
   if (number > 0.5) return true;
   else return false;
 }
 
 app.post("/api/upload", upload.single("file"), async (req, res) => {
+  console.log("entered");
   const imagePath = path.join(__dirname, req.file.path);
-  const proc = execSync(
-    "python3 " +
-      "prepare_ela_image.py " +
-      imagePath
-  );
+  const proc = execSync("python3 " + "prepare_ela_image.py " + imagePath);
   const results = proc.toString();
   const newResult = JSON.parse(results);
-
+  console.log("script done");
   // console.log(results);
+  console.log("started loading model");
   const predictions = await loadModel(newResult);
+  console.log("model work done");
   res.send({ newResult, predictions });
 });
 
-app.listen(process.env.PORT || 3000,() => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
